65bcc4364ac30d1238608934846eccd7897ef6a2
Amala Mary Paul <amalamarypaul@gmail.com>
2020-03-10 15:03:22 +0530

    Global style and CSS module

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import Styles from "./header.module.css"
 
-export default (props)=><h1>{props.headerText}</h1>
+console.log(Styles);
+
+export default (props)=>(
+  <div className={Styles.container}>
+    <h1>{props.headerText}</h1>
+  </div>
+)
